969f411d885797fc4a013e32813952d3aab5f41d
Sergio Hinojosa <sergio2pi@gmail.com>
2015-01-09 00:41:15 -0300

    Funcion que entrega clusters a partir el centro, documentacion con doxygen

--- NeuroDB/cfunctions/cfsfdp/cfsfdp.c
+++ NeuroDB/cfunctions/cfsfdp/cfsfdp.c
@@ -7,7 +7,7 @@
 #include <string.h>
 #include <math.h>
 
-//TODO Returns array. Now all arrays are defined in python a passed it by parameters. 
+//TODO Returns array. Now all arrays are defined in python a passed it by parameters.
 
 int compare(const void *_a, const void *_b) {
     int *a, *b;
@@ -159,11 +159,16 @@ float get_distance_from_res(PGresult *res, int i, int j)
    \endverbatim
 
   */
-float get_dc(char connect[150], char id_block[20], float percent)
+float get_dc(char connect[], char id_block[], char channel[], float percent)
 {
-    char query[200];
-    strcpy(query,"SELECT spike.p1, spike.p2, spike.p3, spike.id from SPIKE JOIN  segment ON id_segment = segment.id WHERE segment.id_block = ");
+    char query[250];
+    strcpy(query, "SELECT spike.p1, spike.p2, spike.p3 from SPIKE ");
+    strcat(query, "JOIN segment ON id_segment = segment.id ");
+    strcat(query, "JOIN recordingchannel ON id_recordingchannel = recordingchannel.id ");
+    strcat(query, "WHERE segment.id_block = ");
     strcat(query, id_block);
+    strcat(query, " AND recordingchannel.index = ");
+    strcat(query, channel);
 
     PGconn          *conn;
     PGresult        *res;
@@ -183,7 +188,7 @@ float get_dc(char connect[150], char id_block[20], float percent)
     res = PQexec(conn,query);
 
     if (PQresultStatus(res) != PGRES_TUPLES_OK) {
-        puts("We did not get any data!");
+        puts("get_dc: We did not get any data!");
         return 0.0;
     }
 
@@ -228,7 +233,7 @@ float get_dc(char connect[150], char id_block[20], float percent)
    \endverbatim
 
   */
-int get_local_density(char connect[150], char id_block[20], float dc, double* local_density, char kernel[20])
+int get_local_density(char connect[], char id_block[], char channel[], float dc, double* local_density, char kernel[20])
 //TODO: Parameter size
 {
 
@@ -236,7 +241,7 @@ int get_local_density(char connect[150], char id_block[20], float dc, double* lo
     PGresult        *res;
     int             rec_count;
 
-    char query[200];
+    char query[250];
     int i, j;
     double distance;
 
@@ -246,8 +251,13 @@ int get_local_density(char connect[150], char id_block[20], float dc, double* lo
         return 1;
     }
 
-    strcpy(query,"SELECT spike.p1, spike.p2, spike.p3 from SPIKE JOIN  segment ON id_segment = segment.id WHERE segment.id_block = ");
+    strcpy(query, "SELECT spike.p1, spike.p2, spike.p3 from SPIKE ");
+    strcat(query, "JOIN segment ON id_segment = segment.id ");
+    strcat(query, "JOIN recordingchannel ON id_recordingchannel = recordingchannel.id ");
+    strcat(query, "WHERE segment.id_block = ");
     strcat(query, id_block);
+    strcat(query, " AND recordingchannel.index = ");
+    strcat(query, channel);
 
     conn = PQconnectdb(connect);
 
@@ -260,7 +270,7 @@ int get_local_density(char connect[150], char id_block[20], float dc, double* lo
     res = PQexec(conn,query);
 
     if (PQresultStatus(res) != PGRES_TUPLES_OK) {
-        puts("We did not get any data!");
+        puts("get_local_density: We did not get any data!");
         return 2;
     }
 
@@ -304,7 +314,7 @@ int get_local_density(char connect[150], char id_block[20], float dc, double* lo
   distance between the point i and any other point with higher density.
 
   @param connect, string of data connect. See example.
-  @param id_block, id project block where this function calculates the dc.
+  @param id_block, id project block.
   @param rho, array from get_local_density.
   @param delta, array to return.
   @param size, array lenght.
@@ -326,7 +336,7 @@ int get_local_density(char connect[150], char id_block[20], float dc, double* lo
    \endverbatim
 
   */
-int get_distance_to_higher_density(char connect[], char id_block[], double* rho, double* delta){
+int get_distance_to_higher_density(char connect[], char id_block[], char channel[], double* rho, double* delta){
 
     PGconn          *conn;
     PGresult        *res;
@@ -334,11 +344,16 @@ int get_distance_to_higher_density(char connect[], char id_block[], double* rho,
 
     double dist;
     double tmp;
-    char query[200];
+    char query[250];
     int i, j, k, flag;
 
-    strcpy(query,"SELECT spike.p1, spike.p2, spike.p3 from SPIKE JOIN  segment ON id_segment = segment.id WHERE segment.id_block = ");
+    strcpy(query, "SELECT spike.p1, spike.p2, spike.p3 from SPIKE ");
+    strcat(query, "JOIN segment ON id_segment = segment.id ");
+    strcat(query, "JOIN recordingchannel ON id_recordingchannel = recordingchannel.id ");
+    strcat(query, "WHERE segment.id_block = ");
     strcat(query, id_block);
+    strcat(query, " AND recordingchannel.index = ");
+    strcat(query, channel);
 
     conn = PQconnectdb(connect);
 
@@ -350,7 +365,7 @@ int get_distance_to_higher_density(char connect[], char id_block[], double* rho,
     res = PQexec(conn,query);
 
     if (PQresultStatus(res) != PGRES_TUPLES_OK) {
-        puts("We did not get any data!");
+        puts("get_distance_to_higher_density: We did not get any data!");
         return 2;
     }
 
@@ -387,6 +402,195 @@ int get_distance_to_higher_density(char connect[], char id_block[], double* rho,
     PQfinish(conn);
 
     return 0;
+}
+
+/**
+  @brief Select centers of clusters
+
+  @param connect, string of data connect. See example.
+  @param id_block, id project block.
+  @param channel, index of channel.
+  @param centers, array where centers are returned.
+  @param dc, see get_dc .
+
+  @returns Code Error (TODO)
+
+  \verbatim
+      float      dc;
+      char connect[] = "dbname=demo host=192.168.2.2 user=postgres password=postgres";
+      double* centers = (double*)calloc(1026,sizeof(double));
+
+      dc = get_dc(connect, "54", "3", 2);
+
+      get_centers_cluster_dp(connect, "54", "3", centers, dc);
+
+   \endverbatim
+
+  */
+int get_centers_cluster_dp(char connect[], char id_block[], char channel[], double* centers, double dc)
+{
+    PGconn          *conn;
+    PGresult        *res;
+    int             rec_count;
+
+    char query[250];
+    double* local_density;
+    double* distance_to_higher_density;
+    double* gamma;
+    float meanf;
+    int k=0, i;
+
+    conn = PQconnectdb(connect);
+
+    strcpy(query, "SELECT spike.id, spike.p1, spike.p2, spike.p3 from SPIKE ");
+    strcat(query, "JOIN segment ON id_segment = segment.id ");
+    strcat(query, "JOIN recordingchannel ON id_recordingchannel = recordingchannel.id ");
+    strcat(query, "WHERE segment.id_block = ");
+    strcat(query, id_block);
+    strcat(query, " AND recordingchannel.index = ");
+    strcat(query, channel);
+
+    if (PQstatus(conn) == CONNECTION_BAD) {
+        puts("cluster_dp: We were unable to connect to the database");
+        return 1;
+    }
+
+    res = PQexec(conn,query);
+    rec_count = PQntuples(res);
+    local_density = (double*)calloc(rec_count,sizeof(double));
+    distance_to_higher_density = (double*)calloc(rec_count,sizeof(double));
+    gamma = (double*)calloc(rec_count,sizeof(double));
+
+    get_local_density(connect, id_block, channel, dc, local_density, "gaussian");
+    get_distance_to_higher_density(connect, id_block, channel, local_density, distance_to_higher_density);
+
+    Quicksort(distance_to_higher_density, local_density, 0, rec_count-1);
+
+    // gamma is rho*delta
+    for(i=0; i<rec_count; i++)
+    {
+        gamma[i] = i*distance_to_higher_density[i];
+    }
+
+    //points > 5*mean are centers of cluster
+    meanf = mean(gamma, rec_count);
+
+    centers[0] = 0;
+    for(i=0; i<rec_count; i++)
+    {
+        if (gamma[i] > 2.5*meanf)
+        {
+            //sscanf(PQgetvalue(res, i, 0),"%lf",&centers[(int)centers[0]+1]);
+            centers[(int)centers[0]+1] = i;
+            centers[0]++;
+        }
+    }
+    return 0;
+    // centers contains index of spike centers, centers[0] is lenght of center
+}
+
+/**
+  @brief From a center, returns all spikes ids with distance minor than dc to the center
+
+  @param connect, string of data connect. See example.
+  @param id_block, id project block.
+  @param channel, index of channel.
+  @param center, index of the center of cluster to returned
+  @param cluster, array where the cluster are returned, it contains ids of spikes.
+  @param dc, see get_dc .
+
+  @returns Code Error (TODO)
+
+  \verbatim
+      float      dc;
+      char connect[] = "dbname=demo host=192.168.2.2 user=postgres password=postgres";
+      double* centers = (double*)calloc(1026,sizeof(double));
+      double* cluster = (double*)calloc(1026,sizeof(double));
+
+      dc = get_dc(connect, "54", "3", 2);
+
+      get_centers_cluster_dp(connect, "54", "3", centers, dc);
+      get_cluster_dp(connect, "54", "3", centers[2], cluster, dc);
+
+   \endverbatim
+
+  */
+int get_cluster_dp(char connect[], char id_block[], char channel[], double center, double* cluster, double dc)
+{
+    PGconn          *conn;
+    PGresult        *res;
+    int             rec_count;
+
+    char query[250];
+    int i, j;
+    float distance;
+    float x1, x2, y1, y2, z1, z2, aux;
+
+    conn = PQconnectdb(connect);
+
+    strcpy(query, "SELECT spike.id, spike.p1, spike.p2, spike.p3 from SPIKE ");
+    strcat(query, "JOIN segment ON id_segment = segment.id ");
+    strcat(query, "JOIN recordingchannel ON id_recordingchannel = recordingchannel.id ");
+    strcat(query, "WHERE segment.id_block = ");
+    strcat(query, id_block);
+    strcat(query, " AND recordingchannel.index = ");
+    strcat(query, channel);
+
+    if (PQstatus(conn) == CONNECTION_BAD) {
+        puts("cluster_dp: We were unable to connect to the database");
+        return 1;
+    }
+
+    res = PQexec(conn,query);
+    rec_count = PQntuples(res);
+
+    i = 1;
+    for(j=0; j<rec_count; j++)
+    {
+        sscanf(PQgetvalue(res, center, 1),"%f",&x1);
+        sscanf(PQgetvalue(res, center, 2),"%f",&y1);
+        sscanf(PQgetvalue(res, center, 3),"%f",&z1);
+        sscanf(PQgetvalue(res, j, 1),"%f",&x2);
+        sscanf(PQgetvalue(res, j, 2),"%f",&y2);
+        sscanf(PQgetvalue(res, j, 3),"%f",&z2);
+        distance = get_distance(x1, y1, z1, x2, y2, z2);
+
+        if (distance <= dc)
+        {
+            sscanf(PQgetvalue(res, j, 0), "%f", &aux);
+            cluster[i] = (double)aux;
+            i++;
+        }
+        cluster[0] = i;
+    }
+    return 0;
+}
+
+
+int main(int argc, char* argv[])
+{
+    //float dc = 0;
+    int i;
+    double* local_density = (double*)calloc(1026,sizeof(double));
+    double* distance_to_higher_density = (double*)calloc(1026,sizeof(double));
+    //double* gamma = (double*)calloc(1026,sizeof(double));
+    double* centers = (double*)calloc(1026,sizeof(double));
+    double* cluster = (double*)calloc(1026,sizeof(double));
+    double** clusters;
+//
+    float dc = get_dc("dbname=demo host=192.168.2.2 user=postgres password=postgres", "54", "3", 2);
+//
+//    get_local_density("dbname=demo host=192.168.2.2 user=postgres password=postgres", "54", dc, local_density, "gaussian");
+//
+//    get_distance_to_higher_density("dbname=demo host=192.168.2.2 user=postgres password=postgres", "54",local_density, distance_to_higher_density, 1026);
+//
+    get_centers_cluster_dp("dbname=demo host=192.168.2.2 user=postgres password=postgres", "54", "3", centers, dc);
+
+    get_cluster_dp("dbname=demo host=192.168.2.2 user=postgres password=postgres", "54", "3", centers[2], cluster, dc);
+//
+//    clusters = get_cluster_dp("dbname=demo host=192.168.2.2 user=postgres password=postgres", "54", centers);
+
+    return 1;
 }
 
 
@@ -508,139 +712,3 @@ double* get_centers_cluster_dp(char connect[], char id_block[])
 
     return clusters;
 }*/
-
-void get_centers_cluster_dp(char connect[], char id_block[], double* centers)
-{
-    PGconn          *conn;
-    PGresult        *res;
-    int             rec_count;
-
-    char query[200];
-    float dc;
-    double* local_density;
-    double* distance_to_higher_density;
-    double* gamma;
-    float meanf;
-    int k=0, i;
-    
-    conn = PQconnectdb(connect);
-    
-    strcpy(query,"SELECT spike.id, spike.p1, spike.p2, spike.p3 from SPIKE JOIN  segment ON id_segment = segment.id WHERE segment.id_block = ");
-    strcat(query, id_block);
-    
-    if (PQstatus(conn) == CONNECTION_BAD) {
-        puts("cluster_dp: We were unable to connect to the database");
-        return NULL;
-    }
-    
-    res = PQexec(conn,query);
-    rec_count = PQntuples(res);
-    local_density = (double*)calloc(rec_count,sizeof(double));
-    distance_to_higher_density = (double*)calloc(rec_count,sizeof(double));
-    gamma = (double*)calloc(rec_count,sizeof(double));
-    
-    dc = get_dc("dbname=demo host=192.168.2.2 user=postgres password=postgres", "54", 2);
-    
-    get_local_density("dbname=demo host=192.168.2.2 user=postgres password=postgres", "54", dc, local_density, "gaussian");
-    get_distance_to_higher_density("dbname=demo host=192.168.2.2 user=postgres password=postgres", "54",local_density, distance_to_higher_density);
-    
-    Quicksort(distance_to_higher_density, local_density, 0, rec_count-1);
-    
-    // gamma is rho*delta
-    for(i=0; i<rec_count; i++)
-    {
-        gamma[i] = i*distance_to_higher_density[i];
-    }
-    
-    //points > 5*mean are centers of cluster
-    meanf = mean(gamma, rec_count);
-    centers = (double*)malloc(sizeof(double));
-    
-    centers[0] = 0;
-    for(i=0; i<rec_count; i++)
-    {
-        if (gamma[i] > 2.5*meanf)
-        {
-            centers = (double*)realloc(centers, sizeof(double)*(centers[0]+1));
-            //sscanf(PQgetvalue(res, i, 0),"%lf",&centers[(int)centers[0]+1]);
-            centers[(int)centers[0]+1] = i;
-            centers[0]++;
-        }
-    }
-    // centers contains index of spike centers, centers[0] is lenght of center
-    return centers;
-}
-
-void get_cluster_dp(char connect[], char id_block[], double center, double* cluster, double dc)
-{
-    PGconn          *conn;
-    PGresult        *res;
-    int             rec_count;
-
-    char query[200];
-    int i, j;
-    float dc, distance;
-    float x1, x2, y1, y2, z1, z2, aux;
-
-    conn = PQconnectdb(connect);
-
-    strcpy(query,"SELECT spike.id, spike.p1, spike.p2, spike.p3 from SPIKE JOIN  segment ON id_segment = segment.id WHERE segment.id_block = ");
-    strcat(query, id_block);
-
-    if (PQstatus(conn) == CONNECTION_BAD) {
-        puts("cluster_dp: We were unable to connect to the database");
-        return NULL;
-    }
-
-    res = PQexec(conn,query);
-    rec_count = PQntuples(res);
-    
-
-    cluster = (double**)malloc(sizeof(double*));
-    i = 1;
-    for(j=0; j<rec_count; j++)
-    {
-        sscanf(PQgetvalue(res, centers[i], 1),"%f",&x1);
-        sscanf(PQgetvalue(res, centers[i], 2),"%f",&y1);
-        sscanf(PQgetvalue(res, centers[i], 3),"%f",&z1);
-        sscanf(PQgetvalue(res, j, 1),"%f",&x2);
-        sscanf(PQgetvalue(res, j, 2),"%f",&y2);
-        sscanf(PQgetvalue(res, j, 3),"%f",&z2);
-        distance = get_distance(x1, y1, z1, x2, y2, z2);
-        if (distance <= dc)
-        {
-            cluster = (double*)realloc(cluster, sizeof(double)*(i+1));
-            sscanf(PQgetvalue(res, j, 0), "%f", &aux);
-            clusters[i] = (double)aux;
-            i++;
-        }
-        clusters[0] = i;
-    }
-    
-    return clusters;
-		
-}
-
-
-int main(int argc, char* argv[])
-{
-float dc = 0;
-    int i;
-    double* local_density = (double*)calloc(1026,sizeof(double));
-    double* distance_to_higher_density = (double*)calloc(1026,sizeof(double));
-    //double* gamma = (double*)calloc(1026,sizeof(double));
-    double* centers = NULL;
-    double** clusters;
-//
-//    float dc = get_dc("dbname=demo host=192.168.2.2 user=postgres password=postgres", "54", 2);
-//
-//    get_local_density("dbname=demo host=192.168.2.2 user=postgres password=postgres", "54", dc, local_density, "gaussian");
-//
-//    get_distance_to_higher_density("dbname=demo host=192.168.2.2 user=postgres password=postgres", "54",local_density, distance_to_higher_density, 1026);
-//
-    centers = get_centers_cluster_dp("dbname=demo host=192.168.2.2 user=postgres password=postgres", "54");
-//
-//    clusters = get_cluster_dp("dbname=demo host=192.168.2.2 user=postgres password=postgres", "54", centers);
-
-    return 1;
-}
--- test/test8.py
+++ test/test8.py
@@ -8,6 +8,15 @@ import numpy as np
 import ctypes
 import numpy.ctypeslib as npct
 import matplotlib.pyplot as plt
+import psycopg2
+
+import neodb.core
+username = 'postgres'
+password = 'postgres'
+host = '192.168.2.2'
+dbname = 'demo'
+url = 'postgresql://%s:%s@%s/%s'%(username, password, host, dbname)
+dbconn = psycopg2.connect('dbname=%s user=%s password=%s host=%s'%(dbname, username, password, host))
 
 array_1d_double = npct.ndpointer(dtype=np.double, ndim=1, flags='CONTIGUOUS')
 array_1d_int = npct.ndpointer(dtype=np.int64, ndim=1, flags='CONTIGUOUS')
@@ -15,9 +24,41 @@ array_2d_double = npct.ndpointer(dtype=np.double, ndim=2, flags='CONTIGUOUS')
 
 libcd = npct.load_library("cfsfdp", "/home/sergio/iibm/workspace/NeuroDB/NeuroDB/cfunctions/cfsfdp")
 
-libcd.get_centers_cluster_dp.argtypes = [ctypes.c_char_p, ctypes.c_char_p]
-libcd.get_centers_cluster_dp.restype = array_1d_double
+libcd.get_centers_cluster_dp.argtypes = [ctypes.c_char_p, ctypes.c_char_p, ctypes.c_char_p, array_1d_double, ctypes.c_double]
+libcd.get_centers_cluster_dp.restype = ctypes.c_int
 
-a = libcd.get_centers_cluster_dp("dbname=demo host=192.168.2.2 user=postgres password=postgres", "54");
+libcd.get_cluster_dp.argtypes = [ctypes.c_char_p, ctypes.c_char_p, ctypes.c_char_p, ctypes.c_double, array_1d_double, ctypes.c_double]
+libcd.get_cluster_dp.restype = ctypes.c_int
 
+libcd.get_dc.argtypes = [ctypes.c_char_p, ctypes.c_char_p, ctypes.c_char_p, ctypes.c_float]
+libcd.get_dc.restype = ctypes.c_float
+
+connect = "dbname=demo host=192.168.2.2 user=postgres password=postgres"
+id_block = "54"
+channel = "3"
+
+dc = libcd.get_dc(connect, id_block, channel, 2.0)
+cluster1 = np.empty(1026)
+cluster2 = np.empty(1026)
+centers = np.empty(1026)
+
+libcd.get_centers_cluster_dp(connect, id_block, channel, centers, dc)
+libcd.get_cluster_dp(connect, id_block, channel,  centers[1], cluster1, dc)
+libcd.get_cluster_dp(connect, id_block, channel,  centers[2], cluster2, dc)
+
+plt.subplot(2,1,1)
+for i in range(int(cluster1[0])):
+    if i != 0:
+        spikes = neodb.core.spikedb.get_from_db(dbconn, id_block = 54, channel = 3, id = int(cluster1[i]))
+        signal = spikes[0].waveform
+        plt.plot(signal)
+
+plt.subplot(2,1,2)
+for i in range(int(cluster2[0])):
+    if i != 0:
+        spikes = neodb.core.spikedb.get_from_db(dbconn, id_block = 54, channel = 3, id = int(cluster2[i]))
+        signal = spikes[0].waveform
+        plt.plot(signal)
+plt.show()
+    
 pass
